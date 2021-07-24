c63cfc0bab7d608ba5ade2a39e0908ba6fb63947
JorgeIsur <ramirezisur@gmail.com>
2021-07-24 03:06:22 -0500

    added try & except blocks to handle with errors

--- rename.py
+++ rename.py
@@ -5,16 +5,23 @@ Destination = input("Ingresa la ruta del directorio de Destino: ")
 name = input("Ingresa el nombre del TV show: ")
 season = input("Ingresa la temporada: ")
 format = input("Ingresa el formato de los archivos(sin puntos): ")
-files = os.listdir(path)
-files.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
-count=1
+try:   
+    files = os.listdir(path)
+    files.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
+    count=1
+except:
+    print("Error en los directorios.")
 def main():
-    for count,filename in enumerate(files):
-        dst = name+"S"+season+"E"+str(count+1) +"."+format
+    try:
+        for count,filename in enumerate(files):
+            dst = name+"S"+season+"E"+str(count+1) +"."+format
           
-        # rename() function will
-        # rename all the files
-        os.rename(os.path.join(path, filename),  os.path.join(Destination, dst))  
+            # rename() function will
+            # rename all the files
+            os.rename(os.path.join(path, filename),  os.path.join(Destination, dst))  
+    except:
+        print("HA OCURRIDO UN ERROR.")
+
 # Driver Code
 if __name__ == '__main__':
       
