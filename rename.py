166ca26d16d0742673afab5c29319b0db57c9397
JorgeIsur <ramirezisur@gmail.com>
2021-07-24 02:58:13 -0500

    improvements

--- rename.py
+++ rename.py
@@ -1,13 +1,16 @@
 import os
 import re
-path = '/home/isur/Videos/TV Shows/HajimeNoIppo/HajimeNoIppo/Season02/'
-Destination = '/home/isur/Videos/TV Shows/HajimeNoIppo/HajimeNoIppo/Season02/'
+path = input("Ingresa la ruta del directorio a renombrar: ")
+Destination = input("Ingresa la ruta del directorio de Destino: ")
+name = input("Ingresa el nombre del TV show: ")
+season = input("Ingresa la temporada: ")
+format = input("Ingresa el formato de los archivos(sin puntos): ")
 files = os.listdir(path)
 files.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
 count=1
 def main():
     for count,filename in enumerate(files):
-        dst ="HajimeNoIppoS02E" + str(count+1) + ".mkv"
+        dst = name+"S"+season+"E"+str(count+1) +"."+format
           
         # rename() function will
         # rename all the files
